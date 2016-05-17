fb10ad64d52ec86b7559d993d5b15ad7a40126c8
Nikolaos Katsanos <nikolaos.katsanos@gmail.com>
2016-05-17 21:18:09 +0100

    Adding a dummy enum just for a blog post example

---
+++ nk-dummies/src/main/java/com/nikoskatsanos/enums/Weekdays.java
@@ -0,0 +1,10 @@
+package com.nikoskatsanos.enums;
+
+/**
+ * <p>Just a simple enum of weekdays</p>
+ *
+ * @author nikkatsa
+ */
+public enum Weekdays {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY
+}
