f0c46106473bda1695859bc4acdd6943adacf74d
Nikolaos Katsanos <nikolaos.katsanos@gmail.com>
2019-03-03 16:28:33 +0000

    Javadocs and link to blog post

--- nk-dummies/src/main/java/com/nikoskatsanos/spinningthread/SpinningThread.java
+++ nk-dummies/src/main/java/com/nikoskatsanos/spinningthread/SpinningThread.java
@@ -3,10 +3,13 @@ package com.nikoskatsanos.spinningthread;
 import com.nikoskatsanos.jutils.core.threading.NamedThreadFactory;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Simple java program that intends to generate a high CPU utilication thread. Made for blog post purposes <a href="https://nikoskatsanos.com/blog/2019/03/03/identifying-a-high-cpu-java-thread/">Identifying
+ * A High CPU Java Thread</a>
+ */
 public class SpinningThread {
 
     private volatile double random;
@@ -24,14 +27,7 @@ public class SpinningThread {
     public static void main(final String... args) {
         final SpinningThread spinningThread = new SpinningThread();
 
-        final ExecutorService spinner = Executors.newSingleThreadExecutor(new ThreadFactory() {
-            @Override
-            public Thread newThread(final Runnable r) {
-                final Thread t = new Thread(r, "Spinner");
-                t.setDaemon(true);
-                return t;
-            }
-        });
+        final ExecutorService spinner = Executors.newSingleThreadExecutor(new NamedThreadFactory("Spinner", true));
         spinner.execute(spinningThread::spin);
 
         while (true) {
